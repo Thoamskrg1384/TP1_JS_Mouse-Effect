9ad732e9de6498a31f3beeda02bc40fc8b56ede8
Thoams Krg <thoams.krieg@gmail.com>
2024-02-18 19:59:42 +0100

    Fin TP ok

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
+// Version définitive
+const mouses = document.querySelectorAll(".mouse");
+
+window.addEventListener("mousemove", (e) => {
+  mouses.forEach((mouse) => {
+    mouse.style.top = e.pageY + "px";
+    mouse.style.left = e.pageX + "px";
+  });
+});
+
+// Version du corrigé
+// window.addEventListener("mousemove", (e) => {
+//   cursor.style.top = e.pageY + "px";
+//   cursor.style.left = e.pageX + "px";
+// });
+
+// window.addEventListener("mousemove", (e) => {
+//   mouse1.style.top = e.pageY + "px";
+//   mouse1.style.left = e.pageX + "px";
+// });
+
+// window.addEventListener("mousemove", (e) => {
+//   mouse2.style.top = e.pageY + "px";
+//   mouse2.style.left = e.pageX + "px";
+// });
+
+// -----------------------------------------------------------------
+
+//Première version by Thoams
+// const circle1 = document.querySelector(".circle1");
+// window.addEventListener("mousemove", (e) => {
+//   circle1.style.left = e.pageX + "px";
+//   circle1.style.top = e.pageY + "px";
+// });
+// const circle2 = document.querySelector(".circle2");
+// window.addEventListener("mousemove", (e) => {
+//   circle2.style.left = e.pageX + "px";
+//   circle2.style.top = e.pageY + "px";
+// });
+// const circle3 = document.querySelector(".circle3");
+// window.addEventListener("mousemove", (e) => {
+//   circle3.style.left = e.pageX + "px";
+//   circle3.style.top = e.pageY + "px";
+// });
+
 // Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
-const circle1 = document.querySelector(".circle1");
-window.addEventListener("mousemove", (e) => {
-  circle1.style.left = e.pageX + "px";
-  circle1.style.top = e.pageY + "px";
-});
-const circle2 = document.querySelector(".circle2");
-window.addEventListener("mousemove", (e) => {
-  circle2.style.left = e.pageX + "px";
-  circle2.style.top = e.pageY + "px";
-});
-const circle3 = document.querySelector(".circle3");
-window.addEventListener("mousemove", (e) => {
-  circle3.style.left = e.pageX + "px";
-  circle3.style.top = e.pageY + "px";
-});
 // Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
 
 // S'assurer que les liens sont clickables
